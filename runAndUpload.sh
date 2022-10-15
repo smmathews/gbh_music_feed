088442a9da12009ddcbc975359f69ac8457d779b
Shane M Mathews <shane.m.mathews@gmail.com>
2022-10-15 16:53:45 -0400

    wrong py name

--- runAndUpload.sh
+++ runAndUpload.sh
@@ -1,5 +1,5 @@
 python3 -m pip install -r requirements.txt
 python3 jazz_feed.py
 python3 in_concert_feed.py
-python3 bso.py
+python3 bso_feed.py
 aws s3 sync ./feeds/ s3://gbh-feed/ --profile gbhfeed
