25685515d1adaa73e89a7e5cdea3e778683db9cd
Shane M Mathews <shane.m.mathews@gmail.com>
2022-10-15 15:32:16 -0400

    89 not 24

--- jazz_feed.test.py
+++ jazz_feed.test.py
@@ -6,7 +6,7 @@ latest_shows_scraper = __import__('latest_shows_scraper')
 
 class TestFunction(unittest.TestCase):
   def test_jazz_feed(self):
-    result = jazz_feed.GetJazzFeed(atom_file="feeds/jazz_24_7_latest_atom.xml", rss_file="feeds/jazz_24_7_latest_rss.xml")
+    result = jazz_feed.GetJazzFeed(atom_file="feeds/jazz_89_7_latest_atom.xml", rss_file="feeds/jazz_89_7_latest_rss.xml")
     self.assertTrue(result)
     # todo, make sure atom and rss files have been saved
 
